6246d74e817bcee3041648732636c9127f258ac9
DT <DT@DT>
2018-03-18 19:49:38 +0530

    Re-Build Application (New WebInitilizer)

--- MVC87/src/com/dt/spring/mvc/DTWebAppInitializer.java
+++ MVC87/src/com/dt/spring/mvc/DTWebAppInitializer.java
@@ -2,25 +2,25 @@ package com.dt.spring.mvc;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class JLCWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class DTWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
-		// TODO Auto-generated method stub
+
 		System.out.println("get rootConfig");
 		return new Class[] { DTConfig.class };
 	}
 
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		// TODO Auto-generated method stub
+
 		System.out.println("getServlet");
 		return new Class[] { DTConfig.class };
 	}
 
 	@Override
 	protected String[] getServletMappings() {
-		// TODO Auto-generated method stub
+
 		System.out.println("getServletMapping");
 		return new String[] { "*.dt" };
 	}
